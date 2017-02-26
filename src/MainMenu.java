2fafe98e84f4d4d432173301cfa1fd3fee904f8b
Kevin Hoopes <kah4627@truman.edu>
2017-02-26 17:31:56 -0600

    tested implementation of player frame, better variable names

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -4,6 +4,7 @@ import java.awt.event.*;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
+import playerPortal.PlayerMain;
 
 /**
  * Class for the main menu of our DnD repository.
@@ -13,30 +14,38 @@ import javax.imageio.ImageIO;
 public class MainMenu extends JFrame implements ActionListener
 {
 	private static final long serialVersionUID = 1L;
-
-	private JPanel[] rows = new JPanel[3];
-	private JButton[] buttons = new JButton[3];
-	private final String[] buttonNames = {"GM Portal", "Player Portal", "Exit"};
-	private final Dimension buttonDim = new Dimension(200,50);
+	
+	private JPanel[] rows;
+	private JButton[] buttons;
+	private final int FRAME_WIDTH = 634;
+	private final int FRAME_HEIGHT = 384;
+	private final String[] BUTTON_NAMES = {"GM Portal", "Player Portal", "Exit"};
+	private final Dimension BUTTON_DIM = new Dimension(200,50);
+	private final FlowLayout FLOW = new FlowLayout( FlowLayout.CENTER );
 	
 	/**
 	 * Constructor for the main menu of the DnD Repository
 	 */
 	public MainMenu()
 	{
+		//set the title for the frame
 		super("DnD Repository");
+		
+		//initialize panels and buttons
+		rows = new JPanel[3];
+		buttons = new JButton[3];
 		
 		//set the background of the menu
 		try 
 		{
-		    final Image backgroundImage = ImageIO.read(new File("./resources/dndFun.jpg"));
-		    setContentPane(new JPanel(new BorderLayout()) 
+		    final Image backgroundImage = ImageIO.read( new File( "./resources/dndFun.jpg" ) );
+		    setContentPane(new JPanel( new BorderLayout() ) 
 		    {
 				private static final long serialVersionUID = 3219358418679061612L;
 
 				@Override public void paintComponent(Graphics g) 
 				{
-		            g.drawImage(backgroundImage, 0, 0, null);
+		            g.drawImage( backgroundImage, 0, 0, null );
 		        }
 		    });
 		} 
@@ -45,28 +54,28 @@ public class MainMenu extends JFrame implements ActionListener
 		    e.printStackTrace();;
 		}
 		
-		setSize(634, 384);
-		setResizable(false);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		BoxLayout grid = new BoxLayout(getContentPane(), BoxLayout.Y_AXIS);
-		setLayout(grid);
+		BoxLayout grid = new BoxLayout( getContentPane(), BoxLayout.Y_AXIS );
+		
+		setSize( FRAME_WIDTH, FRAME_HEIGHT );
+		setResizable( false );
+		setDefaultCloseOperation( EXIT_ON_CLOSE );
+		setLayout( grid );
 		
 		//initialize buttons, panels, and add them to frame
-		FlowLayout flow = new FlowLayout(FlowLayout.CENTER);
 		for( int i = 0; i < rows.length; i++ )
 		{
-			buttons[i] = new JButton(buttonNames[i]);
-			buttons[i].addActionListener(this);
-			buttons[i].setFont(new Font("Times New Roman", Font.BOLD, 16));
-			buttons[i].setPreferredSize(buttonDim);
+			buttons[i] = new JButton( BUTTON_NAMES[i] );
+			buttons[i].addActionListener( this );
+			buttons[i].setFont( new Font( "Times New Roman", Font.BOLD, 16 ) );
+			buttons[i].setPreferredSize( BUTTON_DIM );
 			rows[i] = new JPanel();
-			rows[i].setOpaque(false);
-			rows[i].setLayout(flow);
-			rows[i].add(buttons[i]);
-			add(rows[i]);
+			rows[i].setOpaque( false );
+			rows[i].setLayout( FLOW );
+			rows[i].add( buttons[i] );
+			add( rows[i] );
 		}
 		
-		setVisible(true);
+		setVisible( true );
 	}
 	
 	/**
@@ -83,13 +92,15 @@ public class MainMenu extends JFrame implements ActionListener
 		//Open the player portal
 		if(ae.getSource() == buttons[1])
 		{
+			PlayerMain playerFrame = new PlayerMain();
+			playerFrame.setVisible( true );
 		}
 		
 		//Exit the program
 		if(ae.getSource() == buttons[2])
 		{
-			System.out.println("Thanks for using our DnD Repository!");
-			System.exit(0);
+			System.out.println( "Thanks for using our DnD Repository!" );
+			System.exit( 0 );
 		}
 	}
 	
--- src/playerPortal/PlayerMain.java
+++ src/playerPortal/PlayerMain.java
@@ -1,5 +1,25 @@
 package playerPortal;
+import javax.swing.*;
+import java.awt.*;
 
-public class PlayerMain {
+public class PlayerMain extends JFrame
+{
+	private final FlowLayout flow = new FlowLayout( FlowLayout.CENTER );
+	private JPanel testPanel = new JPanel();
+	private JButton testButton = new JButton( "Test" );
+	
+	public PlayerMain()
+	{
+		super( "Player Menu" );
+		setSize( 634, 384 );
+		setResizable( false );
+		setDefaultCloseOperation( EXIT_ON_CLOSE );
+		setLayout( flow );
+		
+		testPanel.add( testButton );
+		add( testPanel );
+		
+		setVisible( true );
+	}
 
 }
