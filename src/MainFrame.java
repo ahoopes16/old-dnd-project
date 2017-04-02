5046c76f246db76e1f45e7cb9cb5b9b8b5a479b8
Dustin Roan <jango2106@gmail.com>
2017-04-02 14:08:50 -0500

    Simplified background code and changed image

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -11,8 +11,11 @@ import javax.swing.UIManager;
 public class MainFrame extends JFrame 
 {
 	private static final long serialVersionUID = 2269971701250845501L;
+	private final File backgroundImage = new File(".//resources//dndMainImage.jpg");
 	JLabel background;
 	JButton btn;
+	
+	
 	public MainFrame()
 	{
 		super( "Dungeons and Dragons Character Creator" );
@@ -29,7 +32,7 @@ public class MainFrame extends JFrame
 		}
 		try
 		{
-		background = new JLabel(new ImageIcon(ImageIO.read(new File(".//resources//dndFun.jpg"))));
+		background = new JLabel(new ImageIcon(ImageIO.read(backgroundImage)));
 		}
 		catch(Exception e)
 		{
@@ -38,9 +41,12 @@ public class MainFrame extends JFrame
 		
 		btn = new JButton("Bitch");
 		setContentPane(background);
-		setLayout(new FlowLayout());
+		
+		setLayout(new FlowLayout(FlowLayout.RIGHT));
 		add(btn);
-		setSize( 500, 400 );
+		
+		setSize( 400, 400 );
+		setResizable(false);
 		setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
 		setVisible( true );
 	}
