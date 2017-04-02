fcf7b2ead894f73b91ea33f475f08b15e7598c1d
Kevin Hoopes <kah4627@truman.edu>
2017-04-02 13:06:53 -0500

    added look and feel

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,9 +1,23 @@
 import javax.swing.*;
+
 public class MainFrame extends JFrame 
 {
+	private static final long serialVersionUID = 2269971701250845501L;
+
 	public MainFrame()
 	{
 		super( "Dungeons and Dragons Character Creator" );
+		
+		//set look and feel to Nimbus
+		try 
+		{
+			UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+		} 
+		catch (Exception e) 
+		{
+			System.out.println("Had an issue setting the look and feel.");
+			e.printStackTrace();
+		}
 		setSize( 600,500 );
 		setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
 		setVisible( true );
