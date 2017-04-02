a9ddf998b7bd10fc373ec0a7af7f1fc1219d4c2b
Kevin Hoopes <kah4627@truman.edu>
2017-04-02 18:51:25 -0500

    trying to fix github

---
+++ src/characterIdentifiers/Demographics.java
@@ -0,0 +1,14 @@
+package characterIdentifiers;
+/*
+ * A class that holds the demographics of a character
+ * @author Dustin
+ * @version 27 January 2017
+ */
+public class Demographics 
+{
+	CharacterSize size;
+	int age, weight, heightFt, heightIn;
+	String gender, eyes, hair, skin;
+	
+	
+}
